1777aaa21ce385d9ae8e320c77c8ce0b5e727142
Susan0901 <susanoshrestha522@gmail.com>
2025-11-04 01:38:00 +1100

    feat(dark-mode): Added dark mode as a theme

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,11 +3,14 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import { BrowserRouter } from "react-router";
 import Router from "./routes/Router";
+import ThemeContextProvider from "./contexts/ThemeContextProvider";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
-      <Router />
+      <ThemeContextProvider>
+        <Router />
+      </ThemeContextProvider>
     </BrowserRouter>
   </StrictMode>
 );
--- client/src/pages/Welcome.jsx
+++ client/src/pages/Welcome.jsx
@@ -1,11 +1,17 @@
-import React from 'react'
+import React from "react";
+import { useTheme } from "../hooks/useTheme";
 
 const Welcome = () => {
+  const { themeMode, toggleThemeMode } = useTheme();
+  console.log("Theme Mode", themeMode);
   return (
-    <div>
-      Welcome {import.meta.env.VITE_APP_NAME}
+    <div className="h-screen dark:bg-neutral-900 dark:text-white">
+      <div className="flex flex-col items-start">
+        Welcome {import.meta.env.VITE_APP_NAME}
+        <button onClick={toggleThemeMode} className="bg-green-500 p-2 text-white">Toggle</button>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Welcome
+export default Welcome;
